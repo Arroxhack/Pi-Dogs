0e9a3d7832a843b47700810fa129c697472e21dc
Your Name <julian.sonido@gmail.com>
2022-11-08 18:06:30 -0300

    copiado de david

--- api/src/db.js
+++ api/src/db.js
@@ -3,11 +3,9 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME // AL DEPLOYAR HEROKU NOS VA A DAR EL NOMBRE DE DB_NAME QUE QUIERA
-} = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-let sequelize = 
+let sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
         database: DB_NAME,
@@ -15,26 +13,26 @@ let sequelize =
         host: DB_HOST,
         port: 5432,
         username: DB_USER,
-        password:DB_PASSWORD,
+        password: DB_PASSWORD,
         pool: {
           max: 3,
           min: 1,
           idle: 10000,
         },
         dialectOptions: {
-          ssl:{
+          ssl: {
             require: true,
             // Ref.: https://github.com/brianc/node-postgres/issues/2009
             rejectUnauthorized: false,
           },
           keepAlive: true,
         },
-        ssl: true,  
-    })
-  : new Sequelize(
-    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
-    {logging: false, native: false }
-  )
+        ssl: true,
+      })
+    : new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`,
+        { logging: false, native: false }
+      );
 
 /* const {
   DB_USER, DB_PASSWORD, DB_HOST,
